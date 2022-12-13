be25418cd2630dc7a33c5850a8e95bfdbeec8cf2
Abdurrahman <94189718+99Abdurrahman@users.noreply.github.com>
2022-12-13 10:19:30 +0300

    Abdurrahman
    name ,surname and age

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,32 @@
 // your name surname and age from user
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    
+    knowlage() 
+    
+    return 0;
+    
+}
+knowlage(){
+    
+
+    int age;
+    char name[13],surname[13];
+    
+    printf("Please enter your name: \n");
+    scanf("%s",name);
+    
+    printf("Please enter your surname: \n");
+    scanf("%s",surname);
+       
+    printf("Please enter your age: \n");
+    scanf("%d",&age);
+
+    printf(" your name %s surname %s and age %d",name,surname,age);
+
+
+}
